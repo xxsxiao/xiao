df0b2cb9ec530b809d372db75d62314a7bd0a719
神也眷顾小晓吖 <99113472+xxsxiao@users.noreply.github.com>
2023-12-28 02:58:21 +0800

    Update 刷流量.py

--- 刷流量.py
+++ 刷流量.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # -------------------------------
-# cron "0 0 0 * * *" script-path=xxx.py,tag=匹配cron用
+# cron "0 0 6-23/1 * * *" script-path=xxx.py,tag=匹配cron用
+#6点到23点每1小时循环200次
 # const $ = new Env('流量杀手')
 '''
 by：神也眷顾小晓吖
 修复保活规则，利用系统wget进行下载
-
+12/28
+修复卡死线程问题
 '''
 import requests
 import os
@@ -14,6 +16,8 @@ import glob
 import re
 import subprocess
 import shutil
+import select
+
 print("流量杀手-启动")
 print("正在检查安装依赖")
 def check_dependencies(*dependencies):
@@ -26,8 +30,8 @@ def check_dependencies(*dependencies):
 
 # 检查多个依赖
 check_dependencies("os", "requests", "re", "time", "glob", "subprocess", "shutil")
-print("开始删除旧文件日志 ┄ ┄ ┄ 如果没有日志或者任务未运行请手动重新再运行")
-time.sleep(3)
+print("请手动删除旧文件日志\n没有日志就多重新开启几次")
+time.sleep(5)
 from datetime import datetime
 
 # 定义要遍历的目录路径
@@ -49,8 +53,8 @@ for root, dirs, files in os.walk(directory):
                 first_file_path = os.path.join(dir_path, first_file)
                 if os.path.isfile(first_file_path):
                     print("要删除旧日志文件路径:", first_file_path)
-                    print("要删除旧日志文件名称:", first_file)
-                    os.remove(first_file_path)  # 删除文件
+                    print("要删除旧日志文件名称(可能有很多个日志):", first_file)
+                   # os.remove(first_file_path)  # 删除文件
 print("┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄")
 url = 'http://myip.ipip.net/'
 response = requests.get(url)
@@ -67,31 +71,30 @@ if response.status_code == 200:
 else:
     print(f"获取数据失败: {response.status_code}")
 print("┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄")
-url = 'https://issuecdn.baidupcs.com/issue/netdisk/apk/BaiduNetdiskSetup_wap_share.apk'  # 你的文件URL
-download_count = 0
+url = 'https://issuecdn.baidupcs.com/issue/netdisk/apk/BaiduNetdiskSetup_wap_share.apk'
 
 def download_file(url):
-    global download_count
-    while True:
-        download_count += 1
+    for i in range(1, 201):
         print(f"开始下载: {url}")
-        process = subprocess.Popen(['wget', url, '--progress=bar:force', '-O', '/dev/null'], stderr=subprocess.PIPE, universal_newlines=True)
-        
+        process = subprocess.Popen(['wget', url, '-O', '/dev/null', '-q', '--show-progress'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         print(f"进程PID: {process.pid}")
-        t = time.localtime(time.time())
-        localtime = time.asctime(t)
+        localtime = time.asctime(time.localtime(time.time()))
         print(f"当前时间: {localtime}")
-        print(f"下载次数: {download_count}")
         print("┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄")
 
         while True:
             output = process.stderr.readline()
-            if process.poll() is not None and not output:
+            if output == b'' and process.poll() is not None:
                 break
             if output:
-                print(output, end='', flush=True)
-            time.sleep(0.1)
+                print(output.strip().decode('utf-8'))
 
-        print(f"下载完成。等待5秒后重新开始下载...\n┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄")
-        time.sleep(5)
+        process.communicate()
+        
+        print(f"第 {i} 次下载完成")
+        if i < 200:
+            print("等待5秒后重新开始下载...\n┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄ ┄")
+            time.sleep(5)
+
 download_file(url)
